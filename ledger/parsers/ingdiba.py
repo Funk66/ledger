30c0b6fc554bf7b7617341a481818e1271702434
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2022-04-10 08:59:56 +0200

    Add 'notizen' column to ingdiba parser

--- ledger/parsers/ingdiba.py
+++ ledger/parsers/ingdiba.py
@@ -17,10 +17,11 @@ def read(filename: Path, account: str = "ingdiba") -> List[Transaction]:
                     valuta=str2date(values[1]),
                     type=types[values[3]],
                     subject=values[2].strip(),
-                    reference=values[4].strip(),
-                    saldo=str2float(values[5]),
-                    value=str2float(values[7]),
+                    reference=values[5].strip(),
+                    saldo=str2float(values[6]),
+                    value=str2float(values[8]),
                     account=account,
+                    comment=values[4],
                 )
             )
     return transactions
